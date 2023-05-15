a48d19c50555eb36e959dd5e0ea6281bbdbfecd2
waikau maina <ericmaina.dev@gmail.com>
2023-05-15 22:27:44 +0300

    added some hover transition for the create todos button

--- src/components/TodoCreator.vue
+++ src/components/TodoCreator.vue
@@ -33,6 +33,14 @@
   button {
     padding: 8px 16px;
     border: none;
+    cursor: pointer;
+    transition: all .4s ease-in-out;
+
+    &:hover {
+      background-color: #41b080;
+      color: white;
+    }
+
   }
 }
 </style>
