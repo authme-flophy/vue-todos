6ecd86e5b3d8ed6b5c1dca07b9dd2f59ffcb80dd
waikau maina <ericmaina.dev@gmail.com>
2023-05-15 22:14:51 +0300

    created two routes for home and about

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,11 +1,20 @@
 <template>
-  <div>
-
-  </div>
+  <header>
+    <nav class="container">
+      <div class="branding">
+        <img src="../assets/Vue_Logo_Black.png" alt="vue black logo">
+        <h1>Vue Todos</h1>
+      </div>
+      <ul class="nav-routes">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <script setup>
-
+import { RouterLink } from 'vue-router';
 </script>
 
 <style lang="scss" scoped>
