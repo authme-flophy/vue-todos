8e0be8200f0fc476c9b5f040b060cb2551cbd619
waikau maina <ericmaina.dev@gmail.com>
2023-05-16 12:09:24 +0300

    added a watch function to keep updating the todos in the localstorage

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,14 +1,23 @@
 <template>
   <li>
-    <input type="checkbox" :checked="todo.isCompleted">
+    <input type="checkbox" :checked="todo.isCompleted" @input="$emit('toggle-complete', index)">
     <div class="todo">
-      <input v-if="todo.isEditing" type="text" :value="todo.todo">
-      <span v-else>{{ todo.todo }}</span>
+      <input v-if="todo.isEditing" type="text" :value="todo.todo" @input="$emit('update-todo', $event.target.value, index)">
+      <span v-else :class="{'completed-todo' : todo.isCompleted}">{{ todo.todo }}</span>
     </div>
     <div class="todo-actions">
-      <Icon v-if="todo.isEditing" icon="ph:check-circle" class='icon' color="#41b080" width="22" />
-      <Icon v-else icon="ph:pencil-fill" class='icon' color="#41b080" width="22" />
-      <Icon icon="ph:trash" class='icon' color="#f95e5e" width="22" />
+      <Icon 
+        v-if="todo.isEditing" 
+        icon="ph:check-circle" class='icon' color="#41b080" width="22" @click="$emit('edit-todo', index)"  
+      />
+      <Icon 
+        v-else 
+        icon="ph:pencil-fill" class='icon' color="#41b080" width="22" @click="$emit('edit-todo', index)"
+      />
+      <Icon 
+        icon="ph:trash" class='icon' color="#f95e5e" width="22" 
+        @click="$emit('delete-todo', todo.id)"  
+      />
     </div>
   </li>
 </template>
@@ -21,7 +30,12 @@
       type: Object,
       required: true,
     },
+    index: {
+      type: Number,
+      required: true,
+    }
   })
+  defineEmits(['toggle-complete', 'edit-todo', 'update-todo', 'delete-todo'])
 </script>
 
 <style lang="scss" scoped>
@@ -55,6 +69,10 @@ li {
 
   .todo {
     flex: 1;
+    .completed-todo {
+      text-decoration: line-through;
+      color: #41b080;
+    }
 
     input[type="text"] {
       width: 100%;
--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -1,11 +1,31 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import {uid} from 'uid';
 import TodoCreator from '../components/TodoCreator.vue';
 import TodoItem from '../components/TodoItem.vue';
 import {Icon} from '@iconify/vue';
 
   const todoList = ref([])
+
+  watch(todoList, () => {
+    setTodoListLocalStorage()
+  }, {
+    deep: true,
+  })
+
+  const fetchTodoList = () => {
+    const savedTodoList = JSON.parse(localStorage.getItem("todoList"))
+    if (savedTodoList) {
+      todoList.value = savedTodoList
+    }
+  }
+
+  fetchTodoList();
+
+  const setTodoListLocalStorage = () => {
+    localStorage.setItem("todoList", JSON.stringify(todoList.value))
+  }
+
 
   const createTodo = (todo) => {
     todoList.value.push({
@@ -15,6 +35,23 @@ import {Icon} from '@iconify/vue';
       isEditing: null,
     })
   }
+
+  const toggleTodoComplete = (todoPos) => {
+    todoList.value[todoPos].isCompleted = !todoList.value[todoPos].isCompleted
+  }
+  
+  const toggleEditTodo = (todoPos) => {
+    todoList.value[todoPos].isEditing = !todoList.value[todoPos].isEditing
+  }
+  
+  const updateTodo = (todoVal, todoPos) => {
+    todoList.value[todoPos].todo = todoVal
+  }
+
+  const deleteTodo = (todoId) => {
+    todoList.value = todoList.value.filter(todo => todo.id !== todoId)
+  }
+
 </script>
 
 <template>
@@ -22,7 +59,16 @@ import {Icon} from '@iconify/vue';
     <h1>Create Todo</h1>
     <TodoCreator @create-todo="createTodo"/>
     <ul class="todo-list" v-if="todoList.length > 0">
-      <TodoItem v-for="todo in todoList" :todo="todo" :key="todo.id"/>
+      <TodoItem 
+        v-for="(todo, index) in todoList" 
+        :todo="todo" 
+        :index="index" 
+        :key="todo.id"
+        @toggle-complete="toggleTodoComplete"
+        @edit-todo="toggleEditTodo"
+        @update-todo="updateTodo"
+        @delete-todo="deleteTodo"
+      />
     </ul>
     <p class="todos-msg" v-else>
       <Icon icon="noto-v1:sad-but-relieved-face" width="22"/>
