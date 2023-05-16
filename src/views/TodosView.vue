83b7b4aa68d2befcaa0dec327d672cbc13cbac79
waikau maina <ericmaina.dev@gmail.com>
2023-05-16 10:18:01 +0300

    added icons to each todo and craeated a fallback message incase there are no todos

--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -2,6 +2,8 @@
 import { ref } from 'vue';
 import {uid} from 'uid';
 import TodoCreator from '../components/TodoCreator.vue';
+import TodoItem from '../components/TodoItem.vue';
+import {Icon} from '@iconify/vue';
 
   const todoList = ref([])
 
@@ -19,6 +21,13 @@ import TodoCreator from '../components/TodoCreator.vue';
   <main>
     <h1>Create Todo</h1>
     <TodoCreator @create-todo="createTodo"/>
+    <ul class="todo-list" v-if="todoList.length > 0">
+      <TodoItem v-for="todo in todoList" :todo="todo" />
+    </ul>
+    <p class="todos-msg" v-else>
+      <Icon icon="noto-v1:sad-but-relieved-face" width="22"/>
+      <span>You have no todo's to complete! Add one!</span>
+    </p>
   </main>
 </template>
 
@@ -34,6 +43,22 @@ main {
   h1 {
     margin-bottom: 16px;
     text-align: center;
+  }
+
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
   }
 }
 </style>
